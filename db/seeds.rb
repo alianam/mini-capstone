2bdbc4e4fc9f3ec706b1e02ce1a39dfbdabbb591
alianam <aliana@betterment.com>
2017-08-01 22:35:28 -0400

    Add faker to gemfile and a test line in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+product1 = Product.new({name: Faker::Commerce.product_name, price: Faker::Commerce.price, description: Faker::Commerce.color})
