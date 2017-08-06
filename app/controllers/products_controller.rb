a6691d73c8726857ae0772909bef287d083b4a0c
alianam <aliana@betterment.com>
2017-08-06 11:26:32 -0400

    Add new items and product created page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,16 @@
 class ProductsController < ApplicationController
-  def all_products 
+  def all_products
+    @all_products = Product.all
     render 'all-products.html.erb'
+  end
+
+  def new_product
+    render 'new-product-form.html.erb'
+  end
+
+  def product_created
+    @product1 = Product.new({name: params["product_name"], price: params["product_price"], image: params["product_image"], description: params["product_description"]})
+    @product1.save
+    render 'product-created.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/all-products' => 'products#all_products'
+  get '/new-product-form' => 'products#new_product'
+  post '/product-created' => 'products#product_created'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
