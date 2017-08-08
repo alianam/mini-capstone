6011447631b05255112691da99a4569178c248b5
alianam <aliana@betterment.com>
2017-08-07 21:59:54 -0400

    Adding more RESTful things to this app

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,4 +19,25 @@ class ProductsController < ApplicationController
     @product1.save
     render 'product-created.html.erb'
   end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    render 'edit.html.erb'
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(name: params["product_name"],
+      price: params["product_price"],
+      image: params["product_image"], 
+      description: params["product_description"]
+      )
+    render 'show.html.erb'
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    render 'index.html.erb'
+  end
 end
