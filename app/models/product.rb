ffb85f423202653069af4bf3c7e118fe14a5ff95
alianam <aliana@betterment.com>
2017-08-09 22:36:59 -0400

    Add prices and tax

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  def sale_message
+    if price < 2
+      return "Discount Item!"
+    else
+      return "Everyday Value!"
+    end 
+  end
+
+  def tax
+    (price * 0.09).round(3)
+  end
+
+  def total
+    price + tax
+  end
 end
